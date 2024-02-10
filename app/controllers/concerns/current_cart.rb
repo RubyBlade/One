68bcae2702c1a91796a0b957c94c60755c63fd8f
RubyBlade <aliakseikrukouski@gmail.com>
2024-02-10 22:53:46 +0100

    Added Cart and some functional tests for it

---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,12 @@
+module CurrentCart
+
+  private
+
+    def set_cart
+      @cart = Cart.find(session[:cart_id])
+      puts "This cart has #{cart.line_items.count} line items" # Вывод кол-ва итемов в корзине
+    rescue ActiveRecord::RecordNotFound
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
+end
