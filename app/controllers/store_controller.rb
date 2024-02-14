03fa7a69618fd79141ff5726c7ec9ddd94566135
RubyBlade <aliakseikrukouski@gmail.com>
2024-02-14 21:04:39 +0100

    Cart and func.tests updates

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
   def index
+    session[:counter] ||= 0
+    session[:counter] += 1
+    @counter = session[:counter]
+
    @products = Product.order(:title)
   end
 end
