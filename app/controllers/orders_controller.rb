9eaf722803953d35939e8537848a50377ffb8fdd
RubyBlade <aliakseikrukouski@gmail.com>
2024-03-01 23:05:25 +0100

    Added OrderMailer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,6 +31,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        OrderMailer.received(@order).deliver_later
         format.html { redirect_to store_index_url, notice:'Thank you for your order.' }
         format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
         format.json { render :show, status: :created, location: @order }
